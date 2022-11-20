fda91ad9d76d7996db4cd7d8ce67ed4ae61c7691
oobbb <m15934371449@gmail.com>
2022-11-21 00:20:24 +0800

    second commit

---
+++ module/src/main/cpp/hook.cpp
@@ -0,0 +1,126 @@
+
+#include "hook.h"
+#include <cstring>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/system_properties.h>
+#include <dlfcn.h>
+#include <dobby.h>
+#include <dlfcn.h>
+#include <cstdlib>
+#include <cinttypes>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "backends/imgui_impl_android.h"
+
+
+
+
+
+#define GamePackageName "com.aniplex.fategrandorder"
+
+int isGame(JNIEnv *env, jstring appDataDir) {
+    if (!appDataDir)
+        return 0;
+    const char *app_data_dir = env->GetStringUTFChars(appDataDir, nullptr);
+    int user = 0;
+    static char package_name[256];
+    if (sscanf(app_data_dir, "/data/%*[^/]/%d/%s", &user, package_name) != 2) {
+        if (sscanf(app_data_dir, "/data/%*[^/]/%s", package_name) != 1) {
+            package_name[0] = '\0';
+            LOGW("can't parse %s", app_data_dir);
+            return 0;
+        }
+    }
+    if (strcmp(package_name, GamePackageName) == 0) {
+        LOGI("detect game: %s", package_name);
+        game_data_dir = new char[strlen(app_data_dir) + 1];
+        strcpy(game_data_dir, app_data_dir);
+        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
+        return 1;
+    } else {
+        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
+        return 0;
+    }
+}
+
+
+
+
+
+
+int glHeight, glWidth;
+bool setupimg;
+
+
+
+
+
+HOOKAF(void, Input, void *thiz, void *ex_ab, void *ex_ac) {
+    origInput(thiz, ex_ab, ex_ac);
+    ImGui_ImplAndroid_HandleInputEvent((AInputEvent *)thiz);
+    return;
+}
+
+void SetupImgui() {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.DisplaySize = ImVec2((float) glWidth, (float) glHeight);
+    ImGui_ImplOpenGL3_Init("#version 100");
+    ImGui::StyleColorsLight();
+
+    ImGui::GetStyle().ScaleAllSizes(3.0f);
+}
+
+EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
+EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
+    eglQuerySurface(dpy, surface, EGL_WIDTH, &glWidth);
+    eglQuerySurface(dpy, surface, EGL_HEIGHT, &glHeight);
+
+    if (!setupimg) {
+        SetupImgui();
+        setupimg = true;
+    }
+
+    ImGuiIO &io = ImGui::GetIO();
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui::NewFrame();
+
+
+
+    ImGui::ShowDemoWindow();
+
+    ImGui::EndFrame();
+    ImGui::Render();
+    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    return old_eglSwapBuffers(dpy, surface);
+}
+
+
+
+
+
+
+void *hack_thread(void *arg) {
+    sleep(5);
+    auto eglhandle = dlopen("libunity.so", RTLD_LAZY);
+    auto eglSwapBuffers = dlsym(eglhandle, "eglSwapBuffers");
+    DobbyHook((void*)eglSwapBuffers,(dobby_dummy_func_t)hook_eglSwapBuffers,
+         (dobby_dummy_func_t*)&old_eglSwapBuffers);
+    void *sym_input = DobbySymbolResolver(("/system/lib/libinput.so"), ("_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE"));
+    if (NULL != sym_input) {
+        DobbyHook(sym_input,(dobby_dummy_func_t)myInput,(dobby_dummy_func_t*)&origInput);
+    }
+    LOGI("Draw Done!");
+    return nullptr;
+}
---
+++ module/src/main/cpp/hook.h
@@ -0,0 +1,28 @@
+
+#ifndef ZygiskImGui_HOOK_H
+#define ZygiskImGui_HOOK_H
+
+#include <jni.h>
+
+static int enable_hack;
+static char *game_data_dir = NULL;
+
+int isGame(JNIEnv *env, jstring appDataDir);
+
+void *hack_thread(void *arg);
+
+
+#include <android/log.h>
+
+#define LOG_TAG "Zygisk-ImGui"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+#define HOOKAF(ret, func, ...) \
+    ret (*orig##func)(__VA_ARGS__); \
+    ret my##func(__VA_ARGS__)
+
+
+#endif //ZygiskImGui_HOOK_H
